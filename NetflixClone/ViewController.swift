be8aa017b3501822b56d19a183c58b6ca16554f4
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-11-16 17:13:19 +0300

    Initial Commit

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Danil Peregorodiev on 16.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
