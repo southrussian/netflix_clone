d1a6bff9f863ca964c17f1b715c6cb7358e31623
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-11-18 23:25:39 +0300

    Added images and titles for tab bar items

--- NetflixClone/MainTabBarViewController.swift
+++ NetflixClone/MainTabBarViewController.swift
@@ -18,6 +18,16 @@ class MainTabBarViewController: UITabBarController {
         let searchVC = UINavigationController(rootViewController: SearchViewController())
         let downloadsVC = UINavigationController(rootViewController: DownloadsViewController())
         
+        homeVC.tabBarItem.image = UIImage(systemName: "house.fill")
+        upcomingVC.tabBarItem.image = UIImage(systemName: "play.fill")
+        searchVC.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        downloadsVC.tabBarItem.image = UIImage(systemName: "arror.down.to.line")
+        
+        homeVC.tabBarItem.title = "Home"
+        upcomingVC.tabBarItem.title = "Upcoming"
+        searchVC.tabBarItem.title = "Search"
+        downloadsVC.tabBarItem.title = "Downloads"
+        
         setViewControllers([homeVC, upcomingVC, searchVC, downloadsVC], animated: true)
 
 
